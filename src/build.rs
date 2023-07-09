52587aad41eb0a72bd40b93fe2be85aa17272368
mochi-neko <t.o.e.4315@gmail.com>
2023-07-09 16:55:54 +0900

    Implement mock API of chat with grpcurl

--- src/build.rs
+++ src/build.rs
@@ -1,4 +1,13 @@
+use std::{env, path::PathBuf};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("src/proto/helloworld.proto")?;
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    tonic_build::configure()
+        .build_server(true)
+        .file_descriptor_set_path(out_dir.join("chat_descriptor.bin"))
+        .out_dir(out_dir)
+        .compile(&["src/proto/chat.proto"], &["proto"])
+        .unwrap_or_else(|e| panic!("protobuf compile error: {}", e));
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,94 @@
+use std::pin::Pin;
+
+use async_stream::stream;
+use chat::chat_server::{Chat, ChatServer};
+use tokio::time::Duration;
+use tokio_stream::Stream;
 use tonic::{transport::Server, Request, Response, Status};
 
-use hello_world::greeter_server::{Greeter, GreeterServer};
-use hello_world::{HelloReply, HelloRequest};
+pub mod chat {
+    tonic::include_proto!("chat");
 
-pub mod hello_world {
-    tonic::include_proto!("helloworld");
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("chat_descriptor");
 }
 
 #[derive(Debug, Default)]
-pub struct MyGreeter {}
+pub struct MyChat {}
 
 #[tonic::async_trait]
-impl Greeter for MyGreeter {
-    async fn say_hello(
+impl Chat for MyChat {
+    // grpcurl -plaintext localhost:8000 chat.Chat/CompleteChat {\n "message": "Hello!" \n}
+    async fn complete_chat(
         &self,
-        request: Request<HelloRequest>,
-    ) -> Result<Response<HelloReply>, Status> {
-        println!("Got a request: {:?}", request);
+        request: Request<chat::ChatRequest>,
+    ) -> Result<Response<chat::ChatResponse>, Status> {
+        let address = request.remote_addr();
+        println!(
+            "Got a request to complete chat: {:?} from {:?}",
+            request, address
+        );
 
-        let reply = hello_world::HelloReply {
-            message: format!("Hello {}!", request.into_inner().name).into(),
+        let response = chat::ChatResponse {
+            message: format!("Hello {}!", request.into_inner().message),
         };
 
-        Ok(Response::new(reply))
+        println!(
+            "Responding to complete chat with: {:?} to {:?}",
+            response, address
+        );
+
+        Ok(Response::new(response))
+    }
+
+    type CompleteChatStreamingStream = Pin<
+        Box<dyn Stream<Item = Result<chat::ChatStreamingResponse, Status>> + Send + Sync + 'static>,
+    >;
+
+    async fn complete_chat_streaming(
+        &self,
+        request: Request<chat::ChatRequest>,
+    ) -> Result<Response<Self::CompleteChatStreamingStream>, Status> {
+        let address = request.remote_addr();
+        println!(
+            "Got a request to complete chat streaming: {:?} from {:?}",
+            request, address
+        );
+
+        let message = request.into_inner().message;
+
+        let output_stream = stream! {
+            for i in 0u32..=5 {
+                tokio::time::sleep(Duration::from_secs(1)).await;
+
+                let response = chat::ChatStreamingResponse {
+                    delta: format!("Hello {}! Count: {}", message, i),
+                };
+
+                yield Ok(response);
+            }
+        };
+
+        println!("Responding to complete chat streaming to {:?}.", address);
+
+        Ok(Response::new(
+            Box::pin(output_stream) as Self::CompleteChatStreamingStream
+        ))
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let greeter = MyGreeter::default();
+    let addr = "0.0.0.0:8000".parse()?;
+    let chat = MyChat::default();
+
+    let reflection_server = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(chat::FILE_DESCRIPTOR_SET)
+        .build()?;
 
     Server::builder()
-        .add_service(GreeterServer::new(greeter))
+        .add_service(ChatServer::new(chat))
+        .add_service(reflection_server)
         .serve(addr)
         .await?;
 
